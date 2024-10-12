13c630d2a2c8360fb8a44b96ded6e9766a0f19b1
Darion Richards <darionrichards@outlook.com>
2024-10-12 13:00:11 +0100

    Added Nuget Packages

---
+++ DrUmbracoStarterKit/Migrations/StarterKitPackageMigrationPlan.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Umbraco.Cms.Core.Packaging;
+
+namespace DrUmbracoStarterKit.Migrations
+{
+    public class StarterKitPackageMigrationPlan : PackageMigrationPlan
+    {
+        public StarterKitPackageMigrationPlan()
+            : base("DrUmbracoStarterKit")
+        {
+        }
+
+        protected override void DefinePlan()
+        {
+            To<ImportPackageXmlMigration>(new Guid("3B9BC73F-5116-4664-917E-A59015C749ED"));
+        }
+    } 
+}
