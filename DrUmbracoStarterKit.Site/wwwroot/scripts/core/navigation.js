6d3ba537f762661ff775459a3d69cc6dfcf21caf
Darion Richards <darionrichards@outlook.com>
2025-03-31 18:55:01 +0100

    Added burger menu animations for click event on mobile

--- DrUmbracoStarterKit.Site/wwwroot/scripts/core/navigation.js
+++ DrUmbracoStarterKit.Site/wwwroot/scripts/core/navigation.js
@@ -1,1 +1,13 @@
-console.log("NAV JS LOADED")
+const burgerMenu = document.getElementById("burger-menu")
+
+// Function to handle burgerMenu active state
+const handleActiveClass = () => {
+    if (burgerMenu.classList.contains("active")) {
+        burgerMenu.classList.remove("active")
+    } else {
+        burgerMenu.classList.add("active")
+    }
+}
+
+// Click event for burger nav animation.
+burgerMenu.addEventListener("click", handleActiveClass)
